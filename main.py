0ba0af1e55563c6afd92b38e79e453f971bfed88
devjoseantonio <tonysoftco@gmail.com>
2023-11-01 14:29:39 -0500

    Added press button to continue option

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from random import randint #Importin library to use random numbers
+#Importin library to use random numbers
+from random import randint 
 
 #Rival's pokemon Health Points
 rival_hp =  80
@@ -7,16 +8,20 @@ my_hp = 90
 
 #Battle cycle
 while rival_hp > 0 and my_hp > 0:
+    #Pokemons life
+    print("Rival's pikachu life is: {}, your squirtle's life is {}".format(rival_hp, my_hp))
     #Rival's turn
     print("Your rival's turn")
+    input("Press enter to continue...")
     rival_atk = randint(0, 1)
     #If the rival attack is 0 then uses electro ball otherwise uses thunder wave
     if(rival_atk == 0):
-        print("Pikachu uses Electro Ball")
+        print("Pikachu uses Electro Ball -20")
         my_hp -= 20
     else:
-        print("Pikachu uses Thunder Wave")
+        print("Pikachu uses Thunder Wave -10")
         my_hp -= 10
+    input("Press enter to continue...")
     #Your turn
     print("It's your turn wich attack do you want to use?:")
     print("0. Tackle -- atk 10")
@@ -46,3 +51,8 @@ while rival_hp > 0 and my_hp > 0:
                 #Check if the attack selected is a valid option
                 case _:
                     print("Error: select a number between 0-2")
+    input("Press enter to continue...")
+if rival_hp >= 0:
+    print("Rival's pikachu has fainted, you win !")
+else:
+    print("Your squirtle has fainted. Going to a pokemon center")
