96b058336561362c5dbf03d86ad5cca85a3b6284
devjoseantonio <tonysoftco@gmail.com>
2023-11-03 11:33:37 -0500

    Imported only the function neded from subprocess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Importin library to use random numbers
 from random import randint 
-import subprocess as sp
+from subprocess import run as sprun
 #Rival's pokemon Health Points
 RIVAL_MAXHP = 80
 rival_hp =  RIVAL_MAXHP
@@ -31,18 +31,18 @@ def draw_hp(max_hp: float, hp: float):
 #Battle cycle
 while rival_hp > 0 and my_hp > 0:
     #Clear screen
-    sp.run("clear", shell=True)
+    sprun("clear", shell=True)
     #Pokemons life
     print("Rival's pikachu life is: {}".format(rival_hp))
     draw_hp(RIVAL_MAXHP, rival_hp)
     print("your squirtle's life is {}".format(my_hp))
     draw_hp(MY_MAXHP, my_hp)
     input("Press enter to continue...")
-    sp.run("clear", shell=True)
+    sprun("clear", shell=True)
     #Rival's turn
     print("Your rival's turn")
     input("Press enter to continue...")
-    sp.run("clear", shell=True)
+    sprun("clear", shell=True)
     rival_atk = randint(0, 1)
     #If the rival attack is 0 then uses electro ball otherwise uses thunder wave
     if(rival_atk == 0):
@@ -52,7 +52,7 @@ while rival_hp > 0 and my_hp > 0:
         print("Pikachu uses Thunder Wave -10")
         my_hp -= 10
     input("Press enter to continue...")
-    sp.run("clear", shell=True)
+    sprun("clear", shell=True)
     #Your turn
     print("It's your turn wich attack do you want to use?:")
     print("0. Tackle -- atk 10")
@@ -62,7 +62,7 @@ while rival_hp > 0 and my_hp > 0:
     #Repeats until select a valid option
     while my_atk < 0 or my_atk > 2:
         my_atk = input("Select an option: ")
-        sp.run("clear", shell=True)
+        sprun("clear", shell=True)
         #Check if the attack selected is not a number
         if not my_atk.isnumeric():
             print("Error: Select a numeric option")
@@ -84,7 +84,7 @@ while rival_hp > 0 and my_hp > 0:
                 case _:
                     print("Error: select a number between 0-2")
     input("Press enter to continue...")
-    sp.run("clear", shell=True)
+    sprun("clear", shell=True)
 
 #End of the combat
 if rival_hp < 0:
