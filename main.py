cdfab5e7dd00f0c1b737210b2b3339c1fafb785e
devjoseantonio <tonysoftco@gmail.com>
2023-11-03 13:56:08 -0500

    Aded splash movement

--- main.py
+++ main.py
@@ -71,6 +71,7 @@ def battle_cycle():
         print("0. Tackle -- atk 10")
         print("1. Water gun -- atk 15")
         print("2. Bubble -- atk 20")
+        print("3. Splash -- does nothing")
         my_atk = -1
         #Repeats until select a valid option
         while my_atk < 0 or my_atk > 2:
@@ -92,6 +93,8 @@ def battle_cycle():
                     case 2:
                         print("Squirtle uses Bubble -20")
                         rival_hp -= 20
+                    case 3:
+                        print("Squirtle uses Splash -0")
                     #Check if the attack selected is a valid option
                     case _:
                         print("Error: select a number between 0-2")
